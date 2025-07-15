0411d49855fc7f46ded6dfb66705745da925d0c8
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2025-07-15 15:57:08 -0500

    testing and adding components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import "@/app/global.css";
+import Header from "@/components/header";
+import NavBar from "@/components/navBar";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,7 +14,13 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={`antialiased  bg-[#00003c] text-white`}>{children}</body>
+      <body className={`antialiased  bg-[#00003c] text-white`}>
+          <Header />
+        <div>
+          <NavBar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
---
+++ components/NavBar.tsx
@@ -0,0 +1,3 @@
+export default async function NavBar() {
+    return <div>NavBar</div>
+}
