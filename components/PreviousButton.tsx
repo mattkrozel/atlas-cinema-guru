7e27ec65f7c31d46e4f541247860cbbe61ed15e0
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2025-07-16 10:01:05 -0500

    styled pagination buttons

---
+++ components/PreviousButton.tsx
@@ -0,0 +1,7 @@
+export default async function PreviousButton() {
+    return (
+      <button className={'bg-[#63CFB1] text-[#00003c] rounded-r-3xl p-3 m-0.5 w-28'}>
+        Previous
+      </button>
+    );
+}
